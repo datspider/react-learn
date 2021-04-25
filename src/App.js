4cf09efdeb73af61f1db6fc2370f407f86b8e033
phanxuandat1995@gmail.com <phanxuandat1995@gmail.com>
2021-04-25 14:26:22 -0400

    for state

--- src/App.js
+++ src/App.js
@@ -1,57 +1,42 @@
-import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
 
-function App() {
-  const expenseItem = [
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+  const expenses = [
     {
-      id: "1",
-      title: "Insurance",
-      amount: 182,
-      date: new Date(2021, 2, 12),
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id: "2",
-      title: "Keyboard",
-      amount: 200,
-      date: new Date(2021, 1, 29),
-    },
-    {
-      id: "3",
-      title: "PC",
-      amount: 1099,
-      date: new Date(2021, 5, 21),
-    },
-    {
-      id: "4",
-      title: "Paper",
-      amount: 23,
-      date: new Date(2021, 6, 23),
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
-    <div className="App">
-      <h1>Hi, I am a react app</h1>
-      <p>This is really work</p>
-      <ExpenseItem
-        title={expenseItem[0].title}
-        amount={expenseItem[0].amount}
-        date={expenseItem[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenseItem[1].title}
-        amount={expenseItem[1].amount}
-        date={expenseItem[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenseItem[2].title}
-        amount={expenseItem[2].amount}
-        date={expenseItem[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenseItem[3].title}
-        amount={expenseItem[3].amount}
-        date={expenseItem[3].date}
-      ></ExpenseItem>
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
